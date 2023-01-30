86b02d5d4730a2c8377ea821a5ff0c9a9b41bb8c
RoseCass <rosecassidy1303@gmail.com>
2023-01-29 22:58:46 -0800

    added functions to index, took away unnecessary files

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
+DROP DATABASE IF EXISTS avatar_employees;
+CREATE DATABASE avatar_employees;
 
-
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-
-USE employees;
+USE avatar_employees;
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
@@ -13,9 +11,10 @@ CREATE TABLE employee (
   manager_id INT,
   PRIMARY KEY (id),
     -- FOREIGN KEY
-    FOREIGN KEY(manager_id)
-    -- REFERENCES
+     FOREIGN KEY(manager_id)
     REFERENCES employee(id)
+    ON DELETE SET NULL
+
 
 );
 
@@ -30,21 +29,7 @@ INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Luma",
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Rin", "Cass", 1, 1);
 
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT,
-  PRIMARY KEY (id)
-  -- FOREIGN KEY(department_id)
-  -- REFERENCES department(id)
-);
 
-insert into role(title, salary, department_id) values('Manager (The Avatar)', 5000000, 1);
-insert into role(title, salary, department_id) values('Fire Bender', 1000000, 2);
-insert into role(title, salary, department_id) values('Water Bender', 4000000, 3);
-insert into role(title, salary, department_id) values('Air Bender', 3000000, 4);
-insert into role(title, salary, department_id) values('Earth Bender', 2000000, 5);
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
@@ -57,3 +42,19 @@ values ('Management'),
        ('Water'),
        ('Air'),
        ('Earth');
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+
+insert into role(title, salary, department_id) values('Manager (The Avatar)', 5000000, 1);
+insert into role(title, salary, department_id) values('Fire Bender', 1000000, 2);
+insert into role(title, salary, department_id) values('Water Bender', 4000000, 3);
+insert into role(title, salary, department_id) values('Air Bender', 3000000, 4);
+insert into role(title, salary, department_id) values('Earth Bender', 2000000, 5);
